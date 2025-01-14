dacbe1b484e76f3cf8382442388abe12cbd40797
jaspalxiv@gmail.com <jaspalxiv@gmail.com>
2025-01-14 10:30:41 -0700

    week-2 assignment

---
+++ app/week_2/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <div>
+            <h1>My to week-2</h1>
+            <p><Link href="/">Go to home</Link></p>
+        </div>
+    );
+}
+
+
